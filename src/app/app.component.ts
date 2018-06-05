926a5ffd6a4ac19a2bf6c3610840d20e6af31593
Austin Roberts and Kristi Hwang <kristi@>
2018-06-05 11:08:48 -0700

    completed project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Recipes';
+  
+
+  recipes: Recipe[]=[
+    new Recipe('Pie', 'Crust, filling, topping', 'assemble ingredients and bake'),
+    new Recipe('Cookies', 'Butter, flour, sugar, vanilla extract, chocolate chip(semi sweet), baking powder','preheat oven at 450 F, bake 20 minutes'),
+    new Recipe('Shwarma', 'Pita Bread, block of lamb, spit, tzatziki sauce, tabouleh, hummus', 'assemble ingredients on pita bread then roll up ')
+  ];
 }
